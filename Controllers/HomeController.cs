e715cadadc9a50d3f246a4672c1e62f25ed8e7ea
Nathan Costea <cnlnat@yahoo.com>
2019-11-04 16:23:30 -0500

    Sucessfully made a call to an authorized web API - Giphy.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using GiphyWorld.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,8 +14,29 @@ namespace GiphyWorld.Controllers
     {
         public ActionResult Index()
         {
+            var apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["giphyAPIKey"];
+            var query = "pikachu";
+            var limit = "5";
+            var offset = "0";
+            var rating = "G";
+            var lang = "en";
 
-            return View();
+            // Create the request to the API
+            WebRequest request = WebRequest.Create($"https://api.giphy.com/v1/gifs/search?api_key={apiKey}&q={query}&limit={limit}&offset={offset}&rating={rating}&lang={lang}");
+            // Send the request.
+            WebResponse response = request.GetResponse();
+            // Get back the response stream.
+            Stream stream = response.GetResponseStream();
+            // Make the stream reacheable/accessible.
+            StreamReader reader = new StreamReader(stream);
+            // Human readable response.
+            string responseString = reader.ReadToEnd();
+            // Parses the response string.
+            JObject parsedString = JObject.Parse(responseString);
+            // Map the parsed string to our Pokemon model.
+            GiphySearchResultModel searchResult = parsedString.ToObject<GiphySearchResultModel>();
+
+            return View(searchResult);
         }
 
     }
---
+++ Models/GiphySearchResultModel.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiphyWorld.Models
+{
+    public class Hd
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class DownsizedLarge
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedHeightSmallStill
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Original
+    {
+        public string frames { get; set; }
+        public string hash { get; set; }
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedHeightDownsampled
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class DownsizedStill
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedHeightStill
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class DownsizedMedium
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Downsized
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class PreviewWebp
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class OriginalMp4
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedHeightSmall
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedHeight
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class DownsizedSmall
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Preview
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedWidthDownsampled
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedWidthSmallStill
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedWidthSmall
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class OriginalStill
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class FixedWidthStill
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class Looping
+    {
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+    }
+
+    public class FixedWidth
+    {
+        public string height { get; set; }
+        public string mp4 { get; set; }
+        public string mp4_size { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string webp { get; set; }
+        public string webp_size { get; set; }
+        public string width { get; set; }
+    }
+
+    public class PreviewGif
+    {
+        public string height { get; set; }
+        public string size { get; set; }
+        public string url { get; set; }
+        public string width { get; set; }
+    }
+
+    public class __invalid_type__480wStill
+    {
+        public string url { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+    }
+
+    public class Images
+    {
+        public Hd hd { get; set; }
+        public DownsizedLarge downsized_large { get; set; }
+        public FixedHeightSmallStill fixed_height_small_still { get; set; }
+        public Original original { get; set; }
+        public FixedHeightDownsampled fixed_height_downsampled { get; set; }
+        public DownsizedStill downsized_still { get; set; }
+        public FixedHeightStill fixed_height_still { get; set; }
+        public DownsizedMedium downsized_medium { get; set; }
+        public Downsized downsized { get; set; }
+        public PreviewWebp preview_webp { get; set; }
+        public OriginalMp4 original_mp4 { get; set; }
+        public FixedHeightSmall fixed_height_small { get; set; }
+        public FixedHeight fixed_height { get; set; }
+        public DownsizedSmall downsized_small { get; set; }
+        public Preview preview { get; set; }
+        public FixedWidthDownsampled fixed_width_downsampled { get; set; }
+        public FixedWidthSmallStill fixed_width_small_still { get; set; }
+        public FixedWidthSmall fixed_width_small { get; set; }
+        public OriginalStill original_still { get; set; }
+        public FixedWidthStill fixed_width_still { get; set; }
+        public Looping looping { get; set; }
+        public FixedWidth fixed_width { get; set; }
+        public PreviewGif preview_gif { get; set; }
+        public __invalid_type__480wStill __invalid_name__480w_still { get; set; }
+    }
+
+    public class User
+    {
+        public string avatar_url { get; set; }
+        public string banner_image { get; set; }
+        public string banner_url { get; set; }
+        public string profile_url { get; set; }
+        public string username { get; set; }
+        public string display_name { get; set; }
+        public bool is_verified { get; set; }
+    }
+
+    public class Onload
+    {
+        public string url { get; set; }
+    }
+
+    public class Onclick
+    {
+        public string url { get; set; }
+    }
+
+    public class Onsent
+    {
+        public string url { get; set; }
+    }
+
+    public class Analytics
+    {
+        public Onload onload { get; set; }
+        public Onclick onclick { get; set; }
+        public Onsent onsent { get; set; }
+    }
+
+    public class Datum
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string slug { get; set; }
+        public string bitly_gif_url { get; set; }
+        public string bitly_url { get; set; }
+        public string embed_url { get; set; }
+        public string username { get; set; }
+        public string source { get; set; }
+        public string title { get; set; }
+        public string rating { get; set; }
+        public string content_url { get; set; }
+        public string source_tld { get; set; }
+        public string source_post_url { get; set; }
+        public int is_sticker { get; set; }
+        public string import_datetime { get; set; }
+        public string trending_datetime { get; set; }
+        public Images images { get; set; }
+        public User user { get; set; }
+        public Analytics analytics { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int total_count { get; set; }
+        public int count { get; set; }
+        public int offset { get; set; }
+    }
+
+    public class Meta
+    {
+        public int status { get; set; }
+        public string msg { get; set; }
+        public string response_id { get; set; }
+    }
+
+    public class GiphySearchResultModel
+    {
+        public List<Datum> data { get; set; }
+        public Pagination pagination { get; set; }
+        public Meta meta { get; set; }
+    }
+}
